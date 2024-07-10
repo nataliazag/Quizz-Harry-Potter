4556fa6a456a6ff87352bab2cd4e65b02b5a269f
Natália Zaguini <162721549+nataliazag@users.noreply.github.com>
2024-07-10 10:02:33 -0300

    Add files via upload
    Subindo projeto

---
+++ quizz.js
@@ -0,0 +1,32 @@
+let selectMenu1 = document.getElementById('selectMenu1')
+let selectMenu2 = document.getElementById('selectMenu2')
+let selectMenu3 = document.getElementById('selectMenu3')
+let resultado = document.getElementById('resultado')
+let somaDosPontos
+
+function Resultado() {
+    somaDosPontos = Number(selectMenu1.value) + Number(selectMenu2.value) + Number(selectMenu3.value)
+
+    resultado.value = somaDosPontos
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
